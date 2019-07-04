41208f419af53c7863ac1ad7a7452d382c55a6a4
gulyayko <a.gulyayko@yandex.ru>
2019-07-04 15:37:41 +0400

    finishing touch

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,7 +13,6 @@ class BasePage(object):
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(10)
 
     def open(self):
         self.browser.get(self.url)
--- test_product_page.py
+++ test_product_page.py
@@ -98,8 +98,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, product_link)
     page.open()
     page.go_to_cart_page()
     cart_page = CartPage(browser, browser.current_url)
